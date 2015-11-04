32c0440cabc5c8138512e100dbd1d4c815cd90cf
Ben Stein <bumstain@gmail.com>
2015-11-04 21:57:35 +0000

    Added template files with HTML, CSS and JS files

---
+++ js/script.js
@@ -0,0 +1,16 @@
+// Plane animation
+$(document).ready(function() {
+  
+ function flyPlane(){
+   scene.css('left', startPos);
+   scene.animate({left: -200}, 7000, 'linear')
+ };
+ 
+  var screenWidth = $(document).width();
+  var startPos = screenWidth;
+  var plane = $('.scene')
+  flyPlane();
+  setInterval(function() {
+    flyPlane();
+  }, 9000);
+});
